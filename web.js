7062c2c042ead2cd2e5907789473dcee9a51618b
Hylton Goss <github@nethost.co.uk>
2013-07-19 23:12:13 +0000

    Changed listening port and ran jslint debugger and removed htmlcontent variable.

--- web.js
+++ web.js
@@ -4,18 +4,17 @@ var HTMLFILE_DEFAULT = "index.html";
 
 var app = express.createServer(express.logger());
 
-// Reads content of file, if file not found error then it reads the default file 
+// Reads content of file, if file not found error then it reads the default file
 var readhtmlcontent = function(htmlfile) {
-    var htmlcontent;
     try {
-	return htmlcontent = fs.readFileSync(htmlfile, "utf-8");
+	return fs.readFileSync(htmlfile, "utf-8");
 	} catch (e) {
 	    if (e.code === 'ENOENT') {
-		return htmlcontent = fs.readFileSync(HTMLFILE_DEFAULT, "utf-8");
+		return fs.readFileSync(HTMLFILE_DEFAULT, "utf-8");
 	    } else {
 		return e.message;
 	    }
-	}	    
+	}
 };
 
 //Checks for command line arguments for a specified file to use
@@ -24,17 +23,17 @@ if (htmlfile.length > 2) {
     htmlfile = htmlfile.slice(2, htmlfile.length);
     htmlfile = htmlfile.toString();
 } else {
-    htmlfile = HTMLFILE_DEFAULT;   
-};
-	
-app.get('/', function(request, response) {    
+    htmlfile = HTMLFILE_DEFAULT;
+}
+
+app.get('/', function(request, response) {
     var htmlcontent = readhtmlcontent(htmlfile);
     var buffer = new Buffer(htmlcontent, "utf-8");
     var htmlresponse = buffer.toString('utf-8');
     response.send(htmlresponse);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
